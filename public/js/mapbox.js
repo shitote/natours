c4f2c95f468886b5b927d2e39662082c1c1bf3ce
steve shitote <steveshitote00@gmail.com>
2023-10-15 09:45:17 +0300

    Added the map to the tour detail page

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,3 +2,48 @@
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic3RldmVzaGl0b3RlIiwiYSI6ImNsbnIxcTUxeDE0NXUyaXM2bmNodXc0ZnIifQ.QodxPjh3bA07N_w8kxTDPQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/steveshitote/clnr21gw000fs01qye96e0sue',
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+location.forEach((loc) => {
+  // Add marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(Map);
+
+  // add a popup
+  new mapboxgl(),
+    Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</>`)
+      .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 250,
+    left: 100,
+    right: 100,
+  },
+});
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,9 +8,7 @@ const stripe = Stripe(
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v2/bookings/checkout-session/${tourId}`,
-    );
+    const session = await axios(`/api/v2/bookings/checkout-session/${tourId}`);
 
     // 2) Create checkout from + chanre credit card
     await stripe.redirectToCheckout({
